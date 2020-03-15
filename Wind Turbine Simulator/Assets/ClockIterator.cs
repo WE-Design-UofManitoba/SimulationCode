3aa78b8112a522a933730676f7a100c68b5d2bf5
Eli Fridlender <EliFridlender@gmail.com>
2020-03-15 10:00:47 -0500

    adding project to git
    Includes wind turbine and graphing assets

---
+++ Wind Turbine Simulator/Assets/ClockIterator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    const double TIMEUNIT;
+
+    InputProcessor inputProcessor = new InputProcessor();
+    TIMEUNIT = inputProcessor.getTime();
+    inputProcessor.getTime();
+    private double time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Wind Turbine Simulator/Assets/InputProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputProcessor : MonoBehaviour
+{
+    private double updateTime = 0.10;
+
+    public InputProcessor(){
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public double getTime(){
+        return updateTime;
+    }
+}
